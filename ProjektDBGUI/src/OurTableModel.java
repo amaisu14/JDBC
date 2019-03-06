39f3b9dda33c8381e6955cc5a0c7c9fd9a0a45dd
amaisu14 <amaisu14@htl-shkoder.com>
2019-03-06 10:54:02 +0100

    Projekt Folder hochgeladen

---
+++ ProjektDBGUI/src/OurTableModel.java
@@ -0,0 +1,28 @@
+
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableModel;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author amaisu14
+ */
+public class OurTableModel extends DefaultTableModel{
+    int columnDisabled=0;
+    public OurTableModel(int columnDisabled){
+        super();
+        this.columnDisabled=columnDisabled;
+    }
+    @Override
+    public boolean isCellEditable(int row,int column){
+        if(column==columnDisabled) 
+            return false;
+        else 
+            return true;
+    }
+}
